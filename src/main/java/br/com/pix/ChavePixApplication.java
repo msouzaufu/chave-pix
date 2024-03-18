6d742478df8e4b8973d2f9a6f969aaf37e7174c3
Matheus <msouzaufu@gmail.com>
2024-03-18 19:40:26 -0300

    initial commit

---
+++ src/main/java/br/com/pix/ChavePixApplication.java
@@ -0,0 +1,13 @@
+package br.com.pix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChavePixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChavePixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pix/ChavePixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChavePixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
